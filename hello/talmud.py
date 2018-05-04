a28684272272f2f777de618a53319b24f1a8f1e2
Joshua Waxman <joshwaxman@gmail.com>
2018-05-04 10:34:09 -0400

    fallback for missing global edges

--- hello/talmud.py
+++ hello/talmud.py
@@ -2,7 +2,120 @@
 from hello.Statement import Statement
 from pymongo import MongoClient
 import datetime
-import hello.graphOutput as graphOutput
+from neo4j.v1 import GraphDatabase, basic_auth
+driver = GraphDatabase.driver("bolt://hobby-iamlocehkkokgbkekbgcgbal.dbs.graphenedb.com:24786",
+                              auth=basic_auth("mivami", "b.jOGYTThIm49J.NCgtoqGY0qrXXajq"))
+
+session = driver.session()
+
+def findStudentRelationships(people):
+    peopleTuple = tuple(sorted(people))
+    if peopleTuple in findStudentRelationships.cache:
+        return findStudentRelationships.cache[peopleTuple]
+
+    edgesOriginal = []  # type: List[Dict[str, str]]
+    relationDict = {}
+    nodesById = dict()
+
+    # include solitary rabbis
+    # so far, only have rabbis if there was a relationship between them
+    # also include solitary rabbis
+
+    query = 'match (r1:EncodedRabbi) where r1.englishName in ' + str(people) + "\n" \
+                                                                               'return r1'
+
+    result = session.run(query)
+
+    for record in result:
+        nodeId = record['r1'].id
+        hebrewName = record['r1']['hebrewName']
+        englishName = record['r1']['englishName']
+        generation = record['r1']['generation']
+
+        nodesById[nodeId] = {'name': englishName, 'hebrewName': hebrewName, 'generation': generation, 'appears': "True"}
+
+    # now include rabbis who have paths
+    query = 'match (r1:EncodedRabbi) where r1.englishName in ' + str(people) + '\n' + \
+            'match (r2:EncodedRabbi) where r2.englishName in ' + str(people) + '\n' + \
+            'match path = (r1)-[relationship2:student]-(r2)' + '\n' + \
+            'return path, relationship2, r1, r2'
+
+    result = session.run(query)
+
+    for record in result:
+        # already added r1 and r2 to nodes, so need not add them
+        # do however add the relationship
+        source = record['relationship2'].start
+        target = record['relationship2'].end
+        relationDict = dict()
+        relationDict['source'] = source
+        relationDict['target'] = target
+        relationDict['type'] = record['relationship2'].type
+        edgesOriginal.append(relationDict)
+
+    # now include rabbis who share a teacher
+    query = 'match (r1:EncodedRabbi) where r1.englishName in ' + str(people) + '\n' + \
+            'match (r2:EncodedRabbi) where id(r1) <> id(r2) and r2.englishName in ' + str(people) + '\n' + \
+            'match (r3:EncodedRabbi) where not r3.englishName in ' + str(people) + '\n' + \
+            'match path = (r1)-[relationship1:student]->(r3)<-[relationship2: student]-(r2)' + '\n' + \
+            'return distinct path, relationship1, relationship2, r1, r2, r3'
+
+    result = session.run(query)
+
+    for record in result:
+        # add the additional rabbi
+        nodeId = record['r3'].id
+        hebrewName = record['r3']['hebrewName']
+        englishName = record['r3']['englishName']
+        generation = record['r3']['generation']
+
+        nodesById[nodeId] = {'name': englishName, 'hebrewName': hebrewName, 'generation': generation, 'appears': "False"}
+
+        r1English = record['r1']['englishName']
+        r2English = record['r2']['englishName']
+
+        source = record['relationship1'].start
+        target = record['relationship1'].end
+        relationDict = dict()
+        relationDict['source'] = source
+        relationDict['target'] = target
+        relationDict['type'] = record['relationship1'].type
+        edgesOriginal.append(relationDict)
+
+        source = record['relationship2'].start
+        target = record['relationship2'].end
+        relationDict = dict()
+        relationDict['source'] = source
+        relationDict['target'] = target
+        relationDict['type'] = record['relationship2'].type
+        edgesOriginal.append(relationDict)
+
+    # add list position to nodesById
+    nodes = []
+    for i, nodeId in enumerate(nodesById.keys()):
+        nodesById[nodeId]['id'] = i
+        nodes.append(nodesById[nodeId])
+
+    # update edges to be by position
+    edges = []  # type: List[Dict[str, str]]
+
+    setSourceTargetType = set()
+
+    for d in edgesOriginal:
+        source = nodesById[d['source']]['id']
+        target = nodesById[d['target']]['id']
+        type = d['type']
+        if (source, target, type) not in setSourceTargetType:
+            element = {'source': source,
+                       'target': target,
+                       'label': type}
+            edges.append(element)
+            setSourceTargetType.add((source, target, type))
+
+    findStudentRelationships.cache[peopleTuple] = edges, nodes
+    return edges, nodes
+
+findStudentRelationships.cache = dict() # type: Dict[Tuple, Tupe]
 
 def getDafYomi():
     client = MongoClient(
@@ -46,7 +159,9 @@ def htmlOutputter(title: str, page: str):
     item = theText[0]
     contents = item['contents']
     student_nodes = item['EncodedNodes']
-    student_edges = item['EncodedEdges']
+    allRabbis = [item['name'] for name in student_nodes]
+    student_edges = findStudentRelationships(allRabbis)
+    #student_edges = item['EncodedEdges']
     local_interaction_nodes = item['LocalInteractionNodes']
     local_interaction_edges = item['LocalInteractionEdges']
     if 'GlobalInteractionNodes' in theText[0]:
@@ -55,7 +170,6 @@ def htmlOutputter(title: str, page: str):
     else:
         global_interaction_nodes = local_interaction_nodes
         global_interaction_edges = local_interaction_edges
-
 
     wrapper += '<a href="https://www.sefaria.org/%s?lang=bi">%s</a></p>' % (title + '.' +str(page), title+" "+str(page)) #Pesachim.7b, Pesachim 7b
 
