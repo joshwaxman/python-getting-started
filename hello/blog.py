707ac9750115c2fa34561e22ca22536d44758885
Joshua Waxman <joshwaxman@gmail.com>
2018-06-06 19:43:43 -0400

    add functional blog

--- hello/blog.py
+++ hello/blog.py
@@ -11,7 +11,12 @@ def getBlogPost():
         html = ''
         for post in db.posts.find().sort('theDate', ASCENDING).limit(10):
             html += mistune.markdown(post['text'])
-    except:
-        html = 'default text'
+            author = post['author']
+            theDate = post['theDate']
+            tags = post['tags']
+            html += ('<p/>posted by ' + author + ' at ' + str(theDate))
+            html += ('<p/>tags ' + str(tags))
+    except Exception as e:
+        html = str(e)
 
     return html
