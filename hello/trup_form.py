4a22dfc0af6a8f9cf969018e5ad03a7fe1c5ec5b
Joshua Waxman <joshwaxman@gmail.com>
2019-01-20 06:25:20 -0500

    add blog

--- hello/trup_form.py
+++ hello/trup_form.py
@@ -4,11 +4,11 @@ class TrupForm(forms.Form):
     pasuk = forms.CharField(
         max_length=2000,
         widget=forms.Textarea(),
-        help_text='Write here your message!'
+        help_text='Enter pasuk (with trup) here.'
     )
 
     def clean(self):
         cleaned_data = super(TrupForm, self).clean()
         pasuk = cleaned_data.get('pasuk')
         if not pasuk:
-            raise forms.ValidationError('You have to write something!')
+            raise forms.ValidationError('You must enter some pasuk!')
