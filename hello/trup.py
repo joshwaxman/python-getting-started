314ee86559ab50640c7589c59092af9a6ac0c1d0
Joshua Waxman <joshwaxman@gmail.com>
2019-06-27 21:29:54 -0400

    point to new cloudmongo store

--- hello/trup.py
+++ hello/trup.py
@@ -745,7 +745,7 @@ def generateTree(text):
 
 def getTree(verse):
     client = MongoClient('mongodb://trup:Talmud1%@cluster0-shard-00-00-tabwi.mongodb.net:27017,cluster0-shard-00-01-tabwi.mongodb.net:27017,cluster0-shard-00-02-tabwi.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
-    db = client.sefaria
+    db = client.db
     iso_trees = db.iso_trees
     iso_trees2 = db.iso_trees2
     trup = db.trup
