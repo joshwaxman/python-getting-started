9191071642e1de5d66852dde57afccf7407145a8
Joshua Waxman <joshwaxman@gmail.com>
2019-07-25 20:03:41 -0400

    add more phonemic classes to clark

--- hello/clark.py
+++ hello/clark.py
@@ -1,5 +1,5 @@
 import os
-#from neo4j.v1 import GraphDatabase, basic_auth
+from neo4j.v1 import GraphDatabase, basic_auth
 from py2neo import Graph
 from py2neo.data import walk
 
@@ -16,62 +16,98 @@ def getClarkFullList():
 
     return html, [], []
 
+def append_graph(nodes, relationships, query):
+    driver = GraphDatabase.driver("bolt://172.104.217.202:7687", session=driver.session())
+    session = driver.session()
+    result = session.run(query)
+    for record in result:
+        for item in record:
+            if type(item).__name__ == 'Node':
+                id = item.id
+                labels = item._labels.copy()
+                properties = item._properties.copy()
+                node = dict(label=labels)
+                for k, v in properties.items():
+                    node[k] = v
+                nodes[id] = node
+            elif type(item).__name__ == 'list': # if multistep relationships
+                item = item[0]
+                id = item.id
+                start = item._start_node.id
+                end = item._end_node.id
+                rel = dict(label=type(item).__name__, source=start, target=end)
+                relationships[id] = rel
+            else: # assume it is a relationship with the __name__ being the rel type
+                id = item.id
+                start = item._start_node.id
+                end = item._end_node.id
+                rel = dict(label=type(item).__name__, source=start, target=end)
+                relationships[id] = rel
+    session.close()
+
+def neoToD3(nodes, relationships):
+    nToI = {key: pos for pos, key in enumerate(nodes)}
+    nds = [value for value in nodes.values()]
+    edges = [dict(source=nToI[v['source']], target=nToI[v['target']], label=v['label']) for v in relationships.values()]
+    return nds, edges
+
 def getClarkShoresh(shoresh: str):
     d = dict()
     g = Graph("bolt://172.104.217.202:7687", auth=("neo4j", "qwerty"))
     #/ browser /:24780 / db / data /
     html = ''
 
-    cs = g.nodes.match('ClarkShoresh', root=shoresh)
-    if len(cs) == 0:
-        return
+    nodes, relationships = {}, {}
+    query = "match r:ClarkShoresh)-[rel]-(p) where r.root='" +  shoresh + "' return r, p, rel"
+    append_graph(nodes, relationships, query)
 
-    i = 0
-    phonemic_classes = set()
-    edges = []
-    for p in cs:
-        html += 'Shoresh: ' + p['root'] + '<br/>'
-        html += 'Meaning: ' + p['meaning'] + '<br/>'
-
-        nodes = []
-        key = p['root'] + ';' + p['meaning']
-        nodeDict = dict()
-        nodeDict[key] = i
-        i += 1
-        nodes.append(dict(root=p['root'], meaning=p['meaning']))
-
-    for j, p in enumerate(cs):
-        rels = g.match(nodes=[p, None], r_type='MEMBER_OF')
-
-        if len(rels) > 0:
-            html += '<br/><b>Phonemic Classes:</b><br/>'
-            for rel in rels:
-                pc = rel.end_node
-
-                html += 'Phonemic class: <a href="' + pc['name'] + '">' + pc['name'] + '</a><br/>'
-                phonemic_classes.add(pc)
-                n = dict(name=pc['name'], group=pc['group'])
-                nodes.append(n)
-                key = n['name'] + ';' + n['group']
-                nodeDict[key] = i
-                edges.append(dict(source=j, target=i, label='MEMBER_OF'))
-                i += 1
-
-    for pc in phonemic_classes:
-        key = pc['name'] + ';' + pc['group']
-        toIndex = nodeDict[key]
-        rels = g.match([None, pc], r_type='MEMBER_OF')
-        for i, rel in enumerate(rels, 1):
-            s = rel.start_node
-            key = s['root'] + ';' + s['meaning']
-            if key in nodeDict.keys():
-                fromIndex = nodeDict[key]
-            else:
-                n = dict(root=s['root'], meaning=s['meaning'])
-                nodes.append(n)
-                fromIndex = len(nodes) - 1
-                nodeDict[key] = fromIndex
-
-            edges.append(dict(source=fromIndex, target=toIndex, label='MEMBER_OF'))
+    nodes, edges = neoToD3(nodes, relationships)
+    # i = 0
+    # phonemic_classes = set()
+    # edges = []
+    # for p in cs:
+    #     html += 'Shoresh: ' + p['root'] + '<br/>'
+    #     html += 'Meaning: ' + p['meaning'] + '<br/>'
+    #
+    #     nodes = []
+    #     key = p['root'] + ';' + p['meaning']
+    #     nodeDict = dict()
+    #     nodeDict[key] = i
+    #     i += 1
+    #     nodes.append(dict(root=p['root'], meaning=p['meaning']))
+    #
+    # for j, p in enumerate(cs):
+    #     rels = g.match(nodes=[p, None], r_type='MEMBER_OF')
+    #
+    #     if len(rels) > 0:
+    #         html += '<br/><b>Phonemic Classes:</b><br/>'
+    #         for rel in rels:
+    #             pc = rel.end_node
+    #
+    #             html += 'Phonemic class: <a href="' + pc['name'] + '">' + pc['name'] + '</a><br/>'
+    #             phonemic_classes.add(pc)
+    #             n = dict(name=pc['name'], group=pc['group'])
+    #             nodes.append(n)
+    #             key = n['name'] + ';' + n['group']
+    #             nodeDict[key] = i
+    #             edges.append(dict(source=j, target=i, label='MEMBER_OF'))
+    #             i += 1
+    #
+    # for pc in phonemic_classes:
+    #     key = pc['name'] + ';' + pc['group']
+    #     toIndex = nodeDict[key]
+    #     rels = g.match([None, pc], r_type='MEMBER_OF')
+    #     for i, rel in enumerate(rels, 1):
+    #         s = rel.start_node
+    #         key = s['root'] + ';' + s['meaning']
+    #         if key in nodeDict.keys():
+    #             fromIndex = nodeDict[key]
+    #         else:
+    #             n = dict(root=s['root'], meaning=s['meaning'])
+    #             nodes.append(n)
+    #             fromIndex = len(nodes) - 1
+    #             nodeDict[key] = fromIndex
+    #
+    #         edges.append(dict(source=fromIndex, target=toIndex, label='MEMBER_OF'))
 
     return html, nodes, edges
